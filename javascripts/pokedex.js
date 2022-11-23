ad0a67d66b8ca165d724fb597a7f387c188b0de2
jhonny <jhondeser@gmail.com>
2022-11-23 20:32:47 +0100

    pokedex final

--- javascripts/pokedex.js
+++ javascripts/pokedex.js
@@ -1,86 +1,78 @@
 let container_img = document.getElementById('pokedex_screen_container')
 let container_data = document.getElementById('pokedex_info_container')
-let input_pokemon = document.getElementById('pokemon_input')
-let btn_pokemon = document.getElementById('pokemon_input_btn')
-let next_btn = document.getElementById('pokedex-btn-next')
-let prev_btn = document.getElementById('pokedex-btn-prev')
-let home_btn = document.getElementById('pokedex-btn-home')
+let search_input = document.getElementById('pokemon_input')
+let btn_search = document.getElementById('pokemon_input_btn')
+let btn_next = document.getElementById('pokedex-btn-next')
+let btn_prev = document.getElementById('pokedex-btn-prev')
+let btn_home = document.getElementById('pokedex-btn-home')
 
 var div_img;
 var div_data;
 var id = 0;
 
-console.log(next_btn);
-
-btn_pokemon.onclick = () => {
-  let pokemon_value = input_pokemon.value
-  let pokemon_path = `https://pokeapi.co/api/v2/pokemon/${pokemon_value}`
-  request(pokemon_path)
-}
-
-next_btn.onclick = () => {
-  if (id == 0) {
-    id = id + 1
-    pokemon_path = `https://pokeapi.co/api/v2/pokemon/${id}`
-    request(pokemon_path)
+btn_next.onclick = () => {
+  if (id == 905) {
+    id = 0;
   } else {
     id = id + 1;
-    pokemon_path = `https://pokeapi.co/api/v2/pokemon/${id}`
-    request(pokemon_path)
+    let id_path = `https://pokeapi.co/api/v2/pokemon/${id}`
+    peticion(id_path)
   }
 }
 
-home_btn.onclick = () => {
-  container_img.removeChild(div_img)
-  container_data.removeChild(div_data)
-  id = 0;
-}
-
-prev_btn.onclick = () => {
+btn_prev.onclick = () => {
   if (id > 0) {
     id = id - 1;
-    pokemon_path = `https://pokeapi.co/api/v2/pokemon/${id}`
-    request(pokemon_path)
+    let id_path = `https://pokeapi.co/api/v2/pokemon/${id}`
+    peticion(id_path)
   }
 }
 
 
+btn_search.onclick = () => {
+  let pokemon_value = search_input.value
+  let path_pokemon = `https://pokeapi.co/api/v2/pokemon/${pokemon_value}`
+  peticion(path_pokemon)
+}
 
-function request(path) {
+function peticion(path) {
   fetch(path)
     .then(response => response.json())
-    .then(data => {
-      print(data, container_img, container_data)
-
-      id = data.id
-    })  
-           
+    .then(data_pokemon => {
+      console.log(data_pokemon);
+      id = data_pokemon.id
+      print(data_pokemon, container_img, container_data)
+    })
 }
 
 function print(obj, container_img, container_data) {
+
   if (container_img.childNodes.length > 1) {
     container_img.removeChild(div_img)
     container_data.removeChild(div_data)
   }
 
   div_img = document.createElement('div');
-  div_data = document.createElement('div')
-
+  div_data = document.createElement('div');
 
   container_img.appendChild(div_img);
   container_data.appendChild(div_data);
 
+  div_img.innerHTML = `<img src="${obj.sprites.front_default} " alt="">`
+  div_data.innerHTML = `
+    <p>${obj.name}</p>
+    <p> id: ${obj.id}</p>
+  `
 
-  div_img.innerHTML = `<img src="${obj.sprites.front_default}" class="poke_img" "alt="">`
-  div_data.innerHTML = `<div class="${obj.id} text_data">
-    Nombre: ${obj.name}<br>
-    Pokemon nº: ${obj.id}<br>
-    Tipo: ${obj.types[0].type.name}<br>
-    Ataque: ${obj.stats[1].base_stat}<br>
-    Defensa: ${obj.stats[2].base_stat}<br>
-    Vitalidad: ${obj.stats[0].base_stat}<br>
-    Velocidad: ${obj.stats[5].base_stat}<br>
-  </div>`
+  let stats = obj.stats
+
+  stats.forEach(el => {
+    let p = document.createElement('p');
+    div_data.appendChild(p);
+
+    p.innerHTML = `
+      
+      ${el.stat.name}: ${el.base_stat}
+    `
+  })
 }
-
-
